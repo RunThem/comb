987523789624fd64bcf0d7495e8dafb25f92182c
RunThem <iccy.fun@outlook.com>
2023-07-17 23:14:02 +0800

    sync origin

--- xmake.lua
+++ xmake.lua
@@ -1,41 +1,47 @@
-set_project('comb')
-
+--- xmake configure
 set_xmakever('2.6.1')
-
-set_version('0.0.1')
-
 add_plugindirs('.plugins')
 
+--- Project name
+set_project('comb')
+
+--- Project version
+set_version('0.0.1')
+
+--- Build mode
 add_rules('mode.debug', 'mode.release')
 
+--- Macro definition
 add_defines('_GNU_SOURCE=1')
 
+--- No warning
 set_warnings('all', 'error')
 
+--- Build C flags
 add_cflags('-std=gnu2x')
 
+--- Unused variables and functions are allowed in debug mode
 if is_mode('debug') then
   add_cflags('-Wno-unused-function')
   add_cflags('-Wno-unused-variable')
 end
 
---- clang block
--- add_cflags('-fblocks')
--- if is_plat('linux') then
---   add_ldflags('-lBlocksRuntime')
---   add_defines('__linux__=1')
--- end
+--- Lambda expressions
+local lambda = false
+if lambda then
+  add_cflags('-fblocks')
+  if is_plat('linux') then
+    add_ldflags('-lBlocksRuntime')
+    add_defines('__linux__=1')
+  end
+end
 
+--- Private repositories
 add_repositories('RunThem https://github.com/RunThem/My-xmake-repo')
-add_requires(
-  -- RunThem repo
-  'libu',
-  'tiny-regex-c',
+add_requires('libu', 'tiny-regex-c')
+add_requires('mimalloc')
 
-  -- xrepo
-  'mimalloc'
-)
-
+--- Project common header file path
 add_includedirs('$(projectdir)/src')
 
 target('comb', function()
@@ -43,12 +49,4 @@ target('comb', function()
   add_files('src/*.c')
 
   add_packages('mimalloc', 'libu', 'tiny-regex-c')
-end)
-
-target('fmt', function()
-  set_kind('phony')
-  set_default('false')
-
-  add_files('src/**.c')
-  set_pcheader('src/**.h')
 end)
